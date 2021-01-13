e4ebfba28fe3c311f67aa483b9db66afe9f1e52f
chenxianlong <794465731@qq.com>
2021-01-13 15:17:04 +0800

    chore(release): publish
     - babel-plugin-style-bind@1.0.1
     - common-pagination-with-dva@1.0.1
     - common-table@1.0.1
     - script-build-canvas@1.0.1
     - script-git-version@1.0.1

--- scripts/release.js
+++ scripts/release.js
@@ -1,16 +1,30 @@
-const child_process = require('child_process')
+const { spawn } = require('child_process')
+const lerna = require.resolve('lerna/cli')
+
+const exec = (command, args, options) => {
+  return new Promise((resolve, reject) => {
+    const child = spawn(command, args, {
+      env: process.env,
+      stdio: 'inherit',
+      ...options,
+    })
+    child.on('error', (error) => {
+      console.error(error)
+      reject(error)
+    })
+    child.on('close', (code) => {
+      if (code) {
+        process.exit(1)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
 
 const privateRegistry = 'http://172.17.3.163:80/'
 const publicRegistry = 'http://nexus.iblockplay.com:8082/repository/npm-hosted/'
 
-child_process.exec(
-  `yarn publish --access public --registry ${privateRegistry}`,
-  (err, stdout, stderr) => {
-    if (err) {
-      console.log(`[learna publish]: ${stdout}`)
-      console.error(`[learna publish]: ${stderr}`)
-    } else {
-      child_process.execSync(`yarn publish --registry ${publicRegistry}`)
-    }
-  },
-)
+exec(lerna, ['publish', '--registry', privateRegistry]).then(() => {
+  exec(lerna, ['publish', '--registry', publicRegistry])
+})
